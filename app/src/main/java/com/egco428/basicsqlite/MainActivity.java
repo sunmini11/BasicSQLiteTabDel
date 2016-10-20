1e91e631d3ba335bb500d4918083469ec0a3eedf
sunmini11 <yaifirst_sunmini@hotmail.com>
2016-10-20 12:26:50 +0700

    tab and delete

--- app/src/main/java/com/egco428/basicsqlite/MainActivity.java
+++ app/src/main/java/com/egco428/basicsqlite/MainActivity.java
@@ -5,14 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends ListActivity {
     private CommentsDataSource dataSource;
+    EditText firstN;
+    EditText LastN;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dataSource = new CommentsDataSource(this);
@@ -20,8 +26,26 @@ public class MainActivity extends ListActivity {
         List<Comment> values = dataSource.getAllComments();
         ArrayAdapter<Comment> adapter = new ArrayAdapter<Comment>(this,android.R.layout.simple_list_item_1,values);
         setListAdapter(adapter);
+        firstN = (EditText)findViewById(R.id.editFirst);
+        LastN = (EditText)findViewById(R.id.editLast);
+        String name = firstN.getText().toString() + LastN.getText().toString();
 
     }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>)getListAdapter();
+        Comment comment = null;
+
+        if(getListAdapter().getCount()>0){
+            comment = (Comment)getListAdapter().getItem(position);
+            dataSource.deleteComment(comment);
+            adapter.remove(comment);
+        }
+    }
+
 
     public void onClick(View view){
         ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>)getListAdapter();
@@ -30,16 +54,17 @@ public class MainActivity extends ListActivity {
             case R.id.addBtn:
                 String[] comments = new String[] {"Good","Cool", "Whatever", "Very nice"};
                 int nextInt = new Random().nextInt(4); //Random comments
-                comment = dataSource.createComment(comments[nextInt]);
+                String name = firstN.getText().toString() + " " + LastN.getText().toString();
+                comment = dataSource.createComment(name);
                 adapter.add(comment);
                 break;
-            case R.id.deleteBtn:
-                if(getListAdapter().getCount()>0){
-                    comment = (Comment)getListAdapter().getItem(0);
-                    dataSource.deleteComment(comment);
-                    adapter.remove(comment);
-                }
-                break;
+//            case R.id.deleteBtn:
+//                if(getListAdapter().getCount()>0){
+//                    comment = (Comment)getListAdapter().getItem(0);
+//                    dataSource.deleteComment(comment);
+//                    adapter.remove(comment);
+//                }
+//                break;
         }
         adapter.notifyDataSetChanged(); //refresh for updating values
     }
